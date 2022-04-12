fa997e23ae69baf758d2716db7be29a4f10f670b
a.stepanova <ps.ovsyanka@gmail.com>
2022-04-12 22:20:49 +0500

    init

--- app/src/main/java/com/example/water_time_kt/DrinkDay.kt
+++ app/src/main/java/com/example/water_time_kt/DrinkDay.kt
@@ -3,6 +3,10 @@ package com.example.water_time_kt
 import java.text.SimpleDateFormat
 import java.util.*
 
-class DrinkDay(val dayResult: Int, val completed:Boolean) {
+class DrinkDay() {
+
+    val id : Long = 0
+    val dayResult : Int = 0
+    val completed : Boolean = false
     val date = SimpleDateFormat("dd.MM").format(Date())
 }
